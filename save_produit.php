839b8b969e6b443f9a9ab17d257504dc8f0dacd5
Exauce Vagheni <exauce.vagheni@gmail.com>
2024-09-14 21:37:00 -0800

    commiting the begin

--- save_produit.php
+++ save_produit.php
@@ -1,5 +1,6 @@
 <?php 
 include("connect_bdd.php");
+
 if(isset($_POST['designation']) AND isset($_POST['categorie'])){
     $req=$con->prepare("INSERT INTO produits(designation,id_categorie) VALUES(?,?)");
     $req->execute(array($_POST['designation'],$_POST['categorie']));
