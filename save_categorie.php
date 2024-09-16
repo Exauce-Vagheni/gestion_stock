d816c4fdb93ef76797d7b20eda2f0e6a65d639cf
Exauce Vagheni <exauce.vagheni@gmail.com>
2024-09-15 21:35:08 -0800

    commiting

--- save_categorie.php
+++ save_categorie.php
@@ -4,6 +4,7 @@ if(isset($_POST['designation'])){
     $req=$con->prepare("INSERT INTO categories(designation) VALUES(?)");
     $req->execute(array($_POST['designation']));
     header("location:categories.php");
+    
 }
 
 ?>
