6abc8376b7a5810ce2f5bbd7c15d3ed53f4cd3d6
Exauce Vagheni <exauce.vagheni@gmail.com>
2024-09-15 21:36:05 -0800

    commiting 3

--- save_categorie.php
+++ save_categorie.php
@@ -1,10 +1,11 @@
 <?php 
 include("connect_bdd.php");
+
 if(isset($_POST['designation'])){
     $req=$con->prepare("INSERT INTO categories(designation) VALUES(?)");
     $req->execute(array($_POST['designation']));
     header("location:categories.php");
-    
+
 }
 
 ?>
